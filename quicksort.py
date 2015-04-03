229172cc045cd822274efe704a628415cf39e6b9
bickybilly <bebefro@gmail.com>
2015-04-03 16:57:55 -0500

    implemented quicksort for array of ints
    will modify to work with whatever inputs we decide to use

---
+++ quicksort.py
@@ -0,0 +1,36 @@
+import random
+def quicksort(array):
+    random.shuffle(array)
+    sort(array, 0, len(array)-1)
+
+def sort(a, lo, hi):
+    if hi <= lo:
+        return
+    p = partition(a, lo, hi) #split array
+    sort(a, lo, p-1) #recursively sort left half
+    sort(a, p+1, hi) #recursively sort right half
+    
+def partition(a, lo, hi):
+    pivot = a[lo] #take first item to be pivot
+    i = lo+1 #start after the first item
+    j = hi
+    done = False
+    while not done:
+        while a[i] < pivot and i < hi:
+            i += 1
+        while a[j] > pivot and j > lo:
+            j -= 1
+        if j <= i:
+            done = True
+        else:
+            a[i],a[j] = a[j],a[i] #exchange higher item with lower item
+    a[lo],a[j] = a[j],a[lo] #put partitioning item in place
+    return j #return index of partitioning item
+
+def main():
+    a = [2,8,4,0,5,6,9]
+    print a
+    quicksort(a)
+    print a
+
+main()
