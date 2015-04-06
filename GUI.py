af9494a9e3c5ef931158aa928090261309f4f59f
joenot443 <joe.crozier@live.ca>
2015-04-06 10:42:52 -0400

    Continued work on GUI, going to start putting everything together next

--- GUI.py
+++ GUI.py
@@ -12,8 +12,10 @@ class MidiFind():
 		self.fpsClock = pygame.time.Clock()
 
 		#Create a screen for us to draw on
-		# global screen
 		self.screen = pygame.display.set_mode((1024, 768))
+
+		self.fadescreen = pygame.display.set_mode((1024, 768))
+
 		#Give us a caption
 		pygame.display.set_caption("MidiFind")
 
@@ -27,8 +29,14 @@ class MidiFind():
 		self.button2 = pygame.sprite.Sprite()
 		self.button3 = pygame.sprite.Sprite()
 
+		self.helptext = pygame.sprite.Sprite()
+
 		self.buttons = pygame.sprite.Group()
+		self.helpcontent = pygame.sprite.Group()
+
+		self.helppage = 1
 		self.buttons.add(self.button1, self.button2, self.button3)
+		self.helpcontent.add(self.helptext)
 
 
 	def main(self):
@@ -42,23 +50,80 @@ class MidiFind():
 
 
 
-
+			#Cycle through all received events
 			for event in pygame.event.get():
+				
+				#If QUIT is called, exit the app
 				if event.type == QUIT:
 					pygame.quit()
 					sys.exit()
-
+				#If the mouse button is pressed up
 				if event.type == pygame.MOUSEBUTTONUP:
+					#Get the mouse position
 					mouse_pos = pygame.mouse.get_pos()
-					print(mouse_pos)
+
+					#BUTTON 1 CLICKED
 					if pygame.Rect(self.button1.rect).collidepoint(mouse_pos):
-						print("ayy")
-						self.menuPage = Page.recognize
-						self.needsRedraw = True;
+						
+						if (self.menuPage == Page.main):
+							self.menuPage = Page.recognize
+							self.needsRedraw = True
+
+
+						elif (self.menuPage == Page.recognize):
+							##Perform recognition
+							print("")
+						elif (self.menuPage == Page.success):
+							print("")
+						elif (self.menuPage == Page.failure):
+							##Failure
+							print("")
+
+					#BUTTON 2 CLICKED
 					if pygame.Rect(self.button2.rect).collidepoint(mouse_pos):
-						print("Button 2")
-					if pygame.Rect(self.button3.rect).collidepoint(mouse_pos):
-						print("Button 3")
+
+						if (self.menuPage == Page.main):
+							##Options
+							print("")
+						elif (self.menuPage == Page.recognize):
+							self.menuPage = Page.help
+							self.needsRedraw = True
+
+						elif (self.menuPage == Page.success):
+							#Back to Main
+							self.menuPage = Page.main
+							self.needsRedraw = True
+						elif (self.menuPage == Page.failure):
+							self.menuPage = Page.recognize
+							self.needsRedraw = True
+						elif (self.menuPage == Page.help):
+							if (self.helppage != 3):
+								self.helppage += 1
+							else:
+								self.helppage = 1
+								self.menuPage = Page.recognize
+							self.needsRedraw = True
+
+					#BUTTON 3 CLICKED
+
+					if pygame.Rect(self.button2.rect).collidepoint(mouse_pos):
+						
+						if (self.menuPage == Page.main):
+							#Exit
+							pygame.quit()
+							sys.exit()							
+						elif (self.menuPage == Page.recognize):
+							#Back to Main
+							self.menuPage = Page.main
+							self.needsRedraw = True
+						elif (self.menuPage == Page.success):
+							#Exit
+							pygame.quit()
+							sys.exit()	
+						elif (self.menuPage == Page.failure):
+							#Failure
+							print("")
+
 
 			pygame.display.update()
 
@@ -84,21 +149,60 @@ class MidiFind():
 			self.button2img = pygame.image.load('./assets/help.png')
 			self.button3img = pygame.image.load('./assets/cancel.png')
 
+		elif page == Page.success:
+
+			backImage = pygame.image.load('./assets/success.png')
+
+			self.button1img = pygame.image.load('./assets/done.png')
+			self.button2img = pygame.image.load('./assets/nexttrack.png')
+			self.button3img = pygame.image.load('./assets/exit.png')
+
+		elif page == Page.failure:
+
+			backImage = pygame.image.load('./assets/failure.png')
+
+			self.button1img = pygame.image.load('./assets/none.png')
+			self.button2img = pygame.image.load('./assets/startover.png')
+			self.button3img = pygame.image.load('./assets/none.png')
+		
+		elif page == Page.help:
+
+			backImage = pygame.image.load('./assets/helpback.png')
+
+			self.button1img = pygame.image.load('./assets/none.png')
+			self.button2img = pygame.image.load('./assets/next.png')
+			self.button3img = pygame.image.load('./assets/back.png')
+
+			if (self.helppage == 3):
+				self.button2img = pygame.image.load('./assets/doneblue.png')
+
+
+			self.helptext.image = pygame.image.load('./assets/helptext'+str(self.helppage)+'.png')
+			print('./assets/helptext'+str(self.helppage)+'.png')
+			self.helptext.rect = [1024/2 - self.helptext.image.get_width()/2, 238, self.helptext.image.get_width(), self.helptext.image.get_height()]
 
 
 
 		#Blit the background onto our screen
 		self.screen.blit(backImage, (0,0))
 		#Set the image and rect for each of our buttons
-		self.button1.image = self.button1img
-		self.button1.rect = [1024/2 - self.button1img.get_width()/2, 522, self.button1img.get_width(), self.button1img.get_height()]
+		if (self.button1img != None):
+			self.button1.image = self.button1img
+			self.button1.rect = [1024/2 - self.button1img.get_width()/2, 522, self.button1img.get_width(), self.button1img.get_height()]
+		if (self.button2img != None):
+			self.button2.image = self.button2img
+			self.button2.rect = [1024/2 - self.button2img.get_width()/2, 588, self.button2img.get_width(), self.button2img.get_height()]
+		if (self.button3img != None):
+			self.button3.image = self.button3img
+			self.button3.rect = [1024/2 - self.button3img.get_width()/2, 655, self.button3img.get_width(), self.button3img.get_height()]
 
-		self.button2.image = self.button2img
-		self.button2.rect = [1024/2 - self.button2img.get_width()/2, 588, self.button2img.get_width(), self.button2img.get_height()]
+		#Draw the help text if we're on the help screen
 
-		self.button3.image = self.button3img
-		self.button3.rect = [1024/2 - self.button3img.get_width()/2, 655, self.button3img.get_width(), self.button3img.get_height()]
+		if (page == Page.help):
+			self.helpcontent.draw(self.screen)
 
+
+		#Draw our buttons
 		self.buttons.draw(self.screen)
 
 		#Flip the display
@@ -112,6 +216,7 @@ class Page(Enum):
 	options = 3
 	success = 4
 	failure = 5
+	help = 6
 
 
 midiFind = MidiFind()
