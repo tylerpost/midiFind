7a696775bc22669e4dc3809677544366543d6954
bickybilly <bebefro@gmail.com>
2015-04-08 11:00:20 -0500

    Quicksorts working, still need to import Song

--- quicksort.py
+++ quicksort.py
@@ -1,6 +1,24 @@
 #sorts Song array by occurences --> uses getOcc()
-from midiSong import Song
 import random
+
+class Song():
+    def __init__(self,  artist,name,fileLoc, occurances):
+        self.artist = artist
+        self.name = name
+        self.fileLocation = fileLoc
+        self.occ = occurances
+
+        def getName(self):
+            return self.name
+
+        def getArtist(self):
+            return self.artist
+
+        def getFileLoc(self):
+            return self.fileLocation
+
+        def getOcc(self):
+            return self.occ
 
 def quicksort(array):
     random.shuffle(array)
@@ -10,9 +28,10 @@ def quicksort(array):
 def sort(a, lo, hi):
     if hi <= lo:
         return
-    p = partition(a, lo, hi) #split array
-    sort(a, lo, p-1) #recursively sort left half
-    sort(a, p+1, hi) #recursively sort right half
+    else:
+        p = partition(a, lo, hi) #split array
+        sort(a, lo, p-1) #recursively sort left half
+        sort(a, p+1, hi) #recursively sort right half
     
 def partition(a, lo, hi):
     pivot = a[lo].occ #take first item to be pivot
@@ -20,9 +39,9 @@ def partition(a, lo, hi):
     j = hi
     done = False
     while not done:
-        while a[i].occ < pivot and i < hi:
+        while i <= j and a[i].occ <= pivot:
             i += 1
-        while a[j].occ > pivot and j > lo:
+        while j >= i and a[j].occ >= pivot:
             j -= 1
         if j <= i:
             done = True
@@ -31,14 +50,16 @@ def partition(a, lo, hi):
     a[lo],a[j] = a[j],a[lo] #put partitioning item in place
     return j #return index of partitioning item
 
-def main():
-    songs = [Song("","","",1),Song("","","",5),Song("","","",3)]
-    for s in songs:
-        print(s.occ)
-    quicksort(songs)
-    print("Sorted")
-    for s in songs:
-        print(s.occ)
-    print("Done")
 
-main()
+##def main():
+##    songs = [Song("","","",1),Song("","","",5),Song("","","",3),Song("","","",3),Song("","","",1),Song("","","",5),Song("","","",6)]
+##    quicksort(songs)
+##    for s in songs:
+##        print(s.occ)
+##    print("Done")
+##    moresongs = [Song("","","",1),Song("","","",5),Song("","","",1),Song("","","",1),Song("","","",1),Song("","","",1),Song("","","",1),Song("","","",1),Song("","","",1),Song("","","",1),Song("","","",1),Song("","","",1),Song("","","",1),Song("","","",1),Song("","","",1),Song("","","",1),Song("","","",1),Song("","","",1),Song("","","",1),Song("","","",1)]
+##    quicksort(moresongs)
+##    for s in moresongs:
+##        print(s.occ)
+##    print("Done")
+##main()
